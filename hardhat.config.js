e01f82f0e66e3dbdf4cad38fbec8a724714835b4
Parth <parthg635@gmail.com>
2023-08-16 09:13:10 +0530

    first

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  paths: {
+    artifacts: './build/contracts',
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/50afaf64debd4815bc909e2b66de2ce9",
+      accounts: ["0xd7b167503b1df67bacb9690ebf7dffd2738310f8bab59139f659624934fc04ab"]
+    }
+  }
+};
